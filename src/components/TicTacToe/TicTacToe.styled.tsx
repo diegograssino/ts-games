96ab19616603323567993cdb87c3ecaf1e2ec924
diego.grassino <diegograssino@gmail.com>
2022-05-17 15:23:04 -0300

    Add board and initial logics

--- src/components/TicTacToe/TicTacToe.styled.tsx
+++ src/components/TicTacToe/TicTacToe.styled.tsx
@@ -2,5 +2,31 @@ import styled from "styled-components";
 
 export const Styled = styled.div`
   /* Styles */
-  margin: 10px 0px 0px 10px;
+  .container {
+    margin: 10px 0px 0px 10px;
+  }
+  .grid3x3 {
+    display: grid;
+    grid-template-columns: repeat(3, 100px);
+    grid-gap: 2px;
+    margin: 5px 0px 0px 0px;
+  }
+
+  .square {
+    background-color: gray;
+    color: white;
+    border: 1px solid black;
+    border-radius: 5px;
+    height: 100px;
+    width: 100px;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 75px;
+    font-weight: bold;
+  }
+  .game-title {
+    margin: 20px 0px 0px 5px;
+  }
 `;
--- src/components/TicTacToe/TicTacToe.tsx
+++ src/components/TicTacToe/TicTacToe.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {Styled} from "./TicTacToe.styled";
 
 function TicTacToe(): JSX.Element {
+  const INITIAL_TURN = "X";
+  const [turn, setTurn] = useState<"X" | "O">(INITIAL_TURN);
+
+  function handleClick() {
+    setTurn(turn === "X" ? "O" : "X");
+  }
+
   return (
     <Styled>
-      <h2>Tic-Tac-Toe</h2>
+      <div className="container">
+        <h2 className="game-title">Tic-Tac-Toe</h2>
+        <p>Turn: {turn}</p>
+        <div className="grid3x3">
+          <div className="square" onClick={handleClick}>
+            X
+          </div>
+          <div className="square" onClick={handleClick}>
+            O
+          </div>
+          <div className="square" onClick={handleClick} />
+          <div className="square" onClick={handleClick} />
+          <div className="square" onClick={handleClick} />
+          <div className="square" onClick={handleClick} />
+          <div className="square" onClick={handleClick} />
+          <div className="square" onClick={handleClick} />
+          <div className="square" onClick={handleClick} />
+        </div>
+      </div>
     </Styled>
   );
 }
